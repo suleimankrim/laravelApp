c7659dac75031d1f39a6a2b29b04a7e1f6b84e03
unknown <sulimankareem90@gmail.com>
2023-01-05 00:30:35 +0300

    create 2

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Posts;
+use App\Http\Requests\StorePostsRequest;
+use App\Http\Requests\UpdatePostsRequest;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostsRequest  $request
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostsRequest $request, Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Posts $posts)
+    {
+        //
+    }
+}
